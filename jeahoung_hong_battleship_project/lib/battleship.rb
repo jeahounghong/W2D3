f96bf8e384a43e12d1c2de7733f8bf03faed16ce
“Jeahoung <jeahounghong@gmail.com>
2022-06-01 12:10:02 -0400

    Finished Battleship

--- jeahoung_hong_battleship_project/lib/battleship.rb
+++ jeahoung_hong_battleship_project/lib/battleship.rb
@@ -3,4 +3,49 @@ require_relative "player"
 
 class Battleship
 
+    attr_reader :board, :player
+
+    def initialize(n)
+        @player = Player.new()
+        @board = Board.new(n)
+        @remaining_misses = @board.size/2
+    end
+
+    def start_game
+        @board.place_random_ships
+        p @board.size/4
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses <= 0
+            p "you lose"
+            return true
+        else
+            return false
+        end
+    end
+
+    def win?
+        if @board.num_ships == 0
+            p "you win"
+            return true
+        else
+            return false
+        end
+    end
+
+    def game_over?
+        return win? || lose?
+    end
+
+    def turn
+        attacked = @board.attack(@player.get_move)
+        @board.print
+        if !attacked
+            @remaining_misses -= 1
+        end
+        p @remaining_misses
+    end
+
 end
