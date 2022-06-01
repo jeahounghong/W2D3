ecda2a1078eff2520d34652733957df9df79d166
“Jeahoung <jeahounghong@gmail.com>
2022-06-01 11:54:26 -0400

    Passed RSPEC Part 2 & 3

--- jeahoung_hong_battleship_project/lib/board.rb
+++ jeahoung_hong_battleship_project/lib/board.rb
@@ -29,18 +29,58 @@ class Board
 
     def attack(position)
         #debugger
-        val = [](position)
+        val = self[position]
         if val == :S
-            []=(position) = :H
+            self[position] = :H
             p "you sunk my battleship!"
             return true
         else
-            Board[position] = :X
+            self[position] = :X
             return false
             # []=(position)
         end
     end
+
+    def place_random_ships
+        arr = [*1..@size]
+        arr_random = arr.sample(@size/4)
+        @grid.each_with_index do |el,i|
+            el.each_with_index do |el2, j|
+                num = i*el.length + j + 1
+                @grid[i][j] = :S if arr_random.include?(num)
+            end
+        end
+    end
+
+    def hidden_ships_grid
+        return @grid.map do |el|
+            el.map do |el2|
+                if el2 == :S
+                    :N
+                else
+                    el2
+                end
+            end
+        end
+    end
+
+    def self.print_grid(grid)
+        grid.each do |el|
+            puts el.join(" ")
+            #REGEX
+        end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(hidden_ships_grid)
+    end
+
+
 end
 
-# bd = Board.new(4)
-# bd.attack([1,1])
+# arr = [[1,2],[1,2]]
+# p arr.map {|el| el.map {|el2| :N}}
--- jeahoung_hong_battleship_project/lib/player.rb
+++ jeahoung_hong_battleship_project/lib/player.rb
@@ -1,3 +1,13 @@
 class Player
+    
 
+    def initialize()
+
+    end
+    
+    def get_move
+        p "enter a position with coordinates separate with a space like '4 7' "
+        coordinates = gets.chomp.split(" ")
+        return coordinates.map {|el| el.to_i}
+    end
 end
